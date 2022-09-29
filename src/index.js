b4a90d598f8adf07ffec3631ca6f9a7b5f1f2654
Angie <nhpss91020@gmail.com>
2022-09-29 17:01:50 +0800

    save change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './NewApp';
+import App from './PracticingWorks/Todo/NewApp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
